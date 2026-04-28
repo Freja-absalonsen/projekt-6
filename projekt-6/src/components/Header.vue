f9834212e0d4aafacff3e411ef1ef4461291b721
caro8847 <carolinestiepernielsen@gmail.com>
2026-04-28 14:29:48 +0200

    sass

--- projekt-6/src/components/Header.vue
+++ projekt-6/src/components/Header.vue
@@ -11,20 +11,20 @@
 <template>
     <header class="header">
         <button
-          class="mobile-nav-toggle"
+          class="header__button"
           :aria-expanded="open.toString()"
           :aria-label="open ? 'Luk menu' : 'Åben menu'"
-          aria-controls="primary-navigation"
+          aria-controls="header__primary-navigation"
           @click="toggle"
         >
-          <span class="burger-line" :class="{ open }"></span>
-          <span class="burger-line" :class="{ open }"></span>
-          <span class="burger-line" :class="{ open }"></span>
+          <span class="header__burger-line" :class="{ open }"></span>
+          <span class="header__burger-line" :class="{ open }"></span>
+          <span class="header__burger-line" :class="{ open }"></span>
         </button>
 
-        <div v-if="open" class="overlay" @click="toggle"></div>
+        <div v-if="open" class="header__overlay" @click="toggle"></div>
 
-        <nav id="primary-navigation" class="side-menu" :class="{ open }">
+        <nav id="header__primary-navigation" class="header__side-menu" :class="{ open }">
           <ul>
             <li><RouterLink :to="{ name: 'about' }" @click="toggle">Vores historie og værdier</RouterLink></li>
             <li><RouterLink :to="{ name: 'kontakt' }" @click="toggle">Kontakt</RouterLink></li>
@@ -33,86 +33,85 @@
     </header>
 </template>
 
-<style scoped>
-.header{
-  background-color: #362110;
-  height: 64px;
-}
-
-.mobile-nav-toggle {
-  position: fixed;
-  top: 20px;
-  right: 20px;
-  z-index: 2000;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 35px;
-  height: 24px;
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-
-.burger-line {
-  height: 3px;
-  width: 100%;
-  background: #FFFED9;
-  transition: all 0.3s ease;
-}
-
-.burger-line.open:nth-child(1) {
-  transform: translateY(9.5px) rotate(45deg);
-}
-
-.burger-line.open:nth-child(2) {
-  opacity: 0;
-}
-
-.burger-line.open:nth-child(3) {
-  transform: translateY(-9.5px) rotate(-45deg);
-}
-
-.overlay {
-  position: fixed;
-  inset: 0;
-  background: rgba(0,0,0,0.4);
-  z-index: 1000;
-}
-
-.side-menu {
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100vh;
-  width: 250px;
-  background: #362110;
-  box-shadow: -2px 0 10px rgba(0,0,0,0.2);
-
-  transform: translateX(100%);
-  transition: transform 0.3s ease;
-
-  z-index: 1001;
-  padding: 80px 20px;
-}
-
-.side-menu.open {
-  transform: translateX(0);
-}
-
-.side-menu ul {
-  list-style: none;
-  padding: 0;
-}
-
-.side-menu li {
-  margin-bottom: 20px;
-}
-
-a{
-  color: #FFFED9;
-  text-decoration: none;
-}
-
+<style lang="scss">
+  .header{
+    background-color: #362110;
+    height: 64px;
+  
+    &__button {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 2000;
+    
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 35px;
+      height: 24px;
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+  
+    &__burger-line {
+      height: 3px;
+      width: 100%;
+      background: #FFFED9;
+      transition: all 0.3s ease;
+    }
+  
+    &__burger-line.open:nth-child(1) {
+      transform: translateY(9.5px) rotate(45deg);
+    }
+  
+    &__burger-line.open:nth-child(2) {
+      opacity: 0;
+    }
+  
+    &__burger-line.open:nth-child(3) {
+      transform: translateY(-9.5px) rotate(-45deg);
+    }
+  
+    &__overlay {
+      position: fixed;
+      inset: 0;
+      background: rgba(0,0,0,0.4);
+      z-index: 1000;
+    }
+  
+    &__side-menu {
+      position: fixed;
+      top: 0;
+      right: 0;
+      height: 100vh;
+      width: 250px;
+      background: #362110;
+      box-shadow: -2px 0 10px rgba(0,0,0,0.2);
+    
+      transform: translateX(100%);
+      transition: transform 0.3s ease;
+    
+      z-index: 1001;
+      padding: 80px 20px;
+    }
+  
+    &__side-menu.open {
+      transform: translateX(0);
+    }
+  
+    &__side-menu ul {
+      list-style: none;
+      padding: 0;
+    }
+  
+    &__side-menu li {
+      margin-bottom: 20px;
+    }
+  
+    a{
+      color: #FFFED9;
+      text-decoration: none;
+    }
+  }
 </style>
