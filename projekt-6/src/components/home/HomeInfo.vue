2c42b082204097add006d1f89aabc0c22cf894b2
Freja-absalonsen <freja.absalonsen.laursen@gmail.com>
2026-04-29 13:19:01 +0200

    tilføjelse af html på home

---
+++ projekt-6/src/components/home/HomeInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="home-info">
+        <div class="home-info__image"></div>
+
+        <div class="home-info__content">
+            <h1 class="home-info__title">Nær mikrobageri</h1>
+            <p class="home-info__text">Et mikrobageri i hjertet af Odense. Her finder du friskbagt brød og kager lavet med omhu og passion. - Et sted du kan kigge forbi på farten, slå et lille smut ind, eller bare være et øjeblik
+            </p>
+
+            <button class="home-info__button">Find vej</button>
+        </div>
+    </section>
+</template>
