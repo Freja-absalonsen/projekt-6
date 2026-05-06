26ade4ef59a64352fefcca28800f322f35ca7c8f
caro8847 <carolinestiepernielsen@gmail.com>
2026-05-06 10:00:05 +0200

    class

--- projekt-6/src/components/about/About.vue
+++ projekt-6/src/components/about/About.vue
@@ -1,25 +1,27 @@
 <template>
     <section class="about">
-        <div class="about__title">
-            <h1>Hvem er vi?</h1>
-        </div>
-        <div class="about__content">
-            <p>
-                Vi er et lille, passioneret mikrobageri i hjertet af Odense. 
-                Hos Nær Mikrobageri handler det om det nære. Nærheden til håndværket og til menneskerne omkring os.
-            </p>
-            <p>
-                Mikrobageriet er ejet af kæresteparret Jesper Lychegaard og Tine Dirckinck-Holmfeld, 
-                som deler en passion for godt brød og ærligt håndværk
-            </p>
-            <div class="about__wrapper">
-                <div class="image-wrapper">
-                    <div class="square-top-right"></div>
-                        <div class="about__images">
-                            <img src="@/assets/about-gang.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
-                            <img src="@/assets/about-kanelsnegl.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
-                        </div>
-                    <div class="square-bottom-left"></div>
+        <div class="about__container">
+            <div class="about__title">
+                <h1>Hvem er vi?</h1>
+            </div>
+            <div class="about__content">
+                <p>
+                    Vi er et lille, passioneret mikrobageri i hjertet af Odense. 
+                    Hos Nær Mikrobageri handler det om det nære. Nærheden til håndværket og til menneskerne omkring os.
+                </p>
+                <p>
+                    Mikrobageriet er ejet af kæresteparret Jesper Lychegaard og Tine Dirckinck-Holmfeld, 
+                    som deler en passion for godt brød og ærligt håndværk
+                </p>
+                <div class="about__wrapper">
+                    <div class="image-wrapper">
+                        <div class="square-top-right"></div>
+                            <div class="about__images">
+                                <img src="@/assets/about-gang.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
+                                <img src="@/assets/about-kanelsnegl.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
+                            </div>
+                        <div class="square-bottom-left"></div>
+                    </div>
                 </div>
             </div>
         </div>
