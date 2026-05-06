bbbb35753143a867ad3e6f46c026e44e8a2dbda2
Freja-absalonsen <freja.absalonsen.laursen@gmail.com>
2026-05-06 10:09:08 +0200

    tilføjelse på homesome

--- projekt-6/src/components/home/HomeInfo.vue
+++ projekt-6/src/components/home/HomeInfo.vue
@@ -1,13 +1,17 @@
 <template>
     <section class="home-info">
-        <div class="home-info__image"></div>
+        <div class="home-info__hero">
+            <video class="home-info__video" autoplay muted loop playsinline>
+                <source src="" type="video/mp4">
+            </video>
+        </div>
 
         <div class="home-info__content">
             <h1 class="home-info__title">Nær mikrobageri</h1>
             <p class="home-info__text">Et mikrobageri i hjertet af Odense. Her finder du friskbagt brød og kager lavet med omhu og passion. - Et sted du kan kigge forbi på farten, slå et lille smut ind, eller bare være et øjeblik
             </p>
 
-            <button class="home-info__button">Find vej</button>
+            <button class="home-info__button">Find vej →</button>
         </div>
     </section>
 </template>
--- projekt-6/src/components/home/HomeSome.vue
+++ projekt-6/src/components/home/HomeSome.vue
@@ -1,8 +1,20 @@
-<script setup>
-</script>
-
 <template>
-    <section>
-        <h2>HEJ MED DIG, DU</h2>
+    <section class="follow">
+        <div class="follow__container">
+            <h3 class="follow__container">Følg med</h3>
+
+            <div class="follow__content">
+                <div class="folloe__visual">
+                    <img class="follow__icon" src="@/assets/home-some-icon.svg" alt="Instagram ikon">
+                    <img class="follow__image" src="@/assets/home-instagram.jpg" alt="Instagram billede">
+                </div>
+            </div>
+
+            <div class="follow__text">
+                <p class="follow__description"></p>
+            </div>
+
+        </div>
+
     </section>
 </template>
