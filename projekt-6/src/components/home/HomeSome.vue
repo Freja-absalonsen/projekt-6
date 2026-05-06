00400467c961ba93a3dd1ba147a605e5ae487464
Freja-absalonsen <freja.absalonsen.laursen@gmail.com>
2026-05-06 11:33:19 +0200

    homeSome

--- projekt-6/src/components/home/HomeSome.vue
+++ projekt-6/src/components/home/HomeSome.vue
@@ -6,18 +6,21 @@
             <div class="divider"></div>
         </div>
         
-        <p class="follow__description"></p>
-
         <div class="follow__content">
-            <div class="folloe__visual">
+            <div class="follow__visual">
                 <img class="follow__icon" src="@/assets/home-some-icon.svg" alt="Instagram ikon">
                 <img class="follow__image" src="@/assets/home-instagram.jpg" alt="Instagram billede">
             </div>
+
+            <div class="follow__text">
+                <p class="follow__description">Se med på vores instagram, for at se de nyeste vare i butikken </p>
+                <button class="follow__button">Følg os →</button>
+
+            </div>
+
         </div>
 
-        <div class="follow__text">
-            <p class="follow__description"></p>
-        </div>
+       
 
         
 
