fc66c02ac2e2382a76cc4e0ad8a8bbe1d0c15143
caro8847 <carolinestiepernielsen@gmail.com>
2026-05-06 10:08:06 +0200

    classes

--- projekt-6/src/components/about/History.vue
+++ projekt-6/src/components/about/History.vue
@@ -1,33 +1,35 @@
 <template>
     <section class="our-history">
-        <div class="divider"></div>
-        <h2>Vores historie</h2>
-        <div class=" divider"></div>
-        <div class="our-history__content">
-            <div class="our-history__top">
-                <p>
-                    Vores rejse begyndte med kærlighed for surdej, godt håndværk og ærlige ingredienser. 
-                    I dag bager vi hver morgen i Overgade – 
-                    med respekt for traditioner og lysten til at skabe noget, der smager af mere.
-                </p>
-                <div class="our-history__wrapper">
-                    <div class="image-wrapper">
-                        <div class="square-top-right"></div>
-                        <img src="@/assets/about-trine.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
-                        <div class="square-bottom-left"></div>
+        <div class="our-history__container">
+            <div class="divider"></div>
+            <h2>Vores historie</h2>
+            <div class=" divider"></div>
+            <div class="our-history__content">
+                <div class="our-history__top">
+                    <p>
+                        Vores rejse begyndte med kærlighed for surdej, godt håndværk og ærlige ingredienser. 
+                        I dag bager vi hver morgen i Overgade – 
+                        med respekt for traditioner og lysten til at skabe noget, der smager af mere.
+                    </p>
+                    <div class="our-history__wrapper">
+                        <div class="image-wrapper">
+                            <div class="square-top-right"></div>
+                            <img src="@/assets/about-trine.jpg" alt="Billede af en smilende, lyshåret kvinde på en cykel.">
+                            <div class="square-bottom-left"></div>
+                        </div>
                     </div>
                 </div>
+                <p>
+                    Drømmen om et eget sted voksede stille over flere år. 
+                    Et sted, hvor vi kunne bage på vores egne præmisser og skabe noget, 
+                    der ikke bare mætter, men også bliver husket.
+                </p>
+                <p>
+                    Mikrobageriet åbnede i Overgade d. 20. februar – i en gade, der fra første øjeblik føltes rigtig. 
+                    Den har en særlig ro og charme, som for os minder om Latinerkvarteret i Aarhus, 
+                    og som indbyder til at sætte tempoet lidt ned.
+                </p>
             </div>
-            <p>
-                Drømmen om et eget sted voksede stille over flere år. 
-                Et sted, hvor vi kunne bage på vores egne præmisser og skabe noget, 
-                der ikke bare mætter, men også bliver husket.
-            </p>
-            <p>
-                Mikrobageriet åbnede i Overgade d. 20. februar – i en gade, der fra første øjeblik føltes rigtig. 
-                Den har en særlig ro og charme, som for os minder om Latinerkvarteret i Aarhus, 
-                og som indbyder til at sætte tempoet lidt ned.
-            </p>
         </div>
     </section>
 </template>
--- projekt-6/src/components/about/Values.vue
+++ projekt-6/src/components/about/Values.vue
@@ -1,18 +1,20 @@
 <template>
     <section class="values">
-        <div class="values__title">
-            <h2>Vores værdier</h2>
-        </div>
-        <div class="values__content">
-            <p>
-                Hver dag bager vi brød med respekt for traditionerne og kærlighed til det gode håndværk. 
-                Vores brød bliver lavet med surdej og lange hævetider.
-                Vi bager ud fra de råvarer vi har, udvalget kan derfor variere fra dag til dag.
-            </p>
-            <p>
-                Navnet Nær er ikke tilfældigt. Det handler om nærvær i processen, i smagen og i mødet med dig, der kigger forbi. 
-                Vi ønsker at skabe et sted, hvor du kan mærke roen, duften af nybagt brød og glæden ved det enkle.
-            </p>
+        <div class="values__container">
+            <div class="values__title">
+                <h2>Vores værdier</h2>
+            </div>
+            <div class="values__content">
+                <p>
+                    Hver dag bager vi brød med respekt for traditionerne og kærlighed til det gode håndværk. 
+                    Vores brød bliver lavet med surdej og lange hævetider.
+                    Vi bager ud fra de råvarer vi har, udvalget kan derfor variere fra dag til dag.
+                </p>
+                <p>
+                    Navnet Nær er ikke tilfældigt. Det handler om nærvær i processen, i smagen og i mødet med dig, der kigger forbi. 
+                    Vi ønsker at skabe et sted, hvor du kan mærke roen, duften af nybagt brød og glæden ved det enkle.
+                </p>
+            </div>
         </div>
     </section>
 </template>
